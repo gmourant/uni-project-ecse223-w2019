99839d98b1edfbc5e04d1892cd4badf0ee092b6c
Georges Mourant <georges.mourant@mail.mcgill.ca>
2019-02-13 13:23:43 -0500

    Create Block223Controller.java

---
+++ src/ca/mcgill/ecse223/block/controller/Block223Controller.java
@@ -0,0 +1,31 @@
+package ca.mcgill.ecse223.block.controller;
+import ca.mcgill.ecse223.block.model.*;
+
+/**
+ * This is a controller interface for our app that implements
+ * the concepts of MVC: Model, View, Controller.
+ */
+public class Block223Controller {
+
+    /**
+     * Constructor for controller.
+     */
+    public Block223Controller(){
+    }
+
+    /**
+     * This method deletes a game in the Block223 application.
+     * @param name is used to find the instance of Game
+     */
+    public static void deleteGame(String name){
+        // Method body to be written
+    }
+
+    /**
+     * This method updates the game settings for an instance of Game.
+     */
+    public static void updateGame(){
+        // Method body to be written
+    }
+
+}
