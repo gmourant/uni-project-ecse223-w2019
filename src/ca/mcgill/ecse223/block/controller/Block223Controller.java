5cc8b95751fbbfae1b8e4b94db4905d54ad78953
Georges Mourant <georges.mourant@mail.mcgill.ca>
2019-02-14 00:07:17 -0500

    Added Update a game

--- src/ca/mcgill/ecse223/block/controller/Block223Controller.java
+++ src/ca/mcgill/ecse223/block/controller/Block223Controller.java
@@ -23,8 +23,21 @@ public class Block223Controller {
 
     /**
      * This method updates the game settings for an instance of Game.
+     * @param name name of the game used to identify the game
+     * @param newName the new game name
+     * @param nrBlocksPerLevel sets the number of blocks for an instance of Game
+     * @param minBallSpeedX sets the minimum x-axis ball speed for an instance of Game
+     * @param minBallSpeedY sets the minimum y-axis ball speed for an instance of Game
+     * @param ballSpeedIncreaseFactor sets the speed increase factor of the ball for an instance of Game
+     * @param maxPaddleLength sets the maximum paddle length for an instance of Game
+     * @param minPaddleLength sets the minimum paddle length for an instance of Game
+     * @param heightPlayArea sets the height of the PlayArea for an instance of Game
+     * @param widthPlayArea sets the width of the PlayArea for an instance of Game\
      */
-    public static void updateGame(){
+    public static void updateGame(String name, String newName, 
+            int nrBlocksPerLevel, int minBallSpeedX, int minBallSpeedY, 
+            double ballSpeedIncreaseFactor, int maxPaddleLength, 
+            int minPaddleLength, int heightPlayArea, int widthPlayArea){
         // Method body to be written
     }
     
