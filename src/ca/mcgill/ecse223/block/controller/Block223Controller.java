5e0f2be90b97b4c36e3b14857e7b0ec7e98e7b54
Mat Bis <mathieu.bissonnette2@mail.mcgill.ca>
2019-02-24 10:50:30 -0500

    Implemented the updateBlock method.

--- src/ca/mcgill/ecse223/block/controller/Block223Controller.java
+++ src/ca/mcgill/ecse223/block/controller/Block223Controller.java
@@ -29,8 +29,72 @@ public class Block223Controller {
 
 	public static void deleteBlock(int id) throws InvalidInputException {
 	}
+	
+	/**
+	 * 
+	 * This method updates a block with new values. It requires a block ID,
+	 * color values (RGB) and the point value of the block.
+	 * 
+	 * @param id	The ID of the desired block.
+	 * @param red	The red component of the block color.
+	 * @param green	The green component of the block color.
+	 * @param blue	The blue component of the block color.
+	 * @param points	The point value of the block.
+	 * 
+	 * @throws InvalidInputException	if red, green, or blue < 0 or > 255 or if 
+	 * 									points < 0 or > 1000
+	 * @throws InvalidInputException	if the block ID does not correspond to an
+	 * 									existing entity.
+	 * 
+	 */
 
 	public static void updateBlock(int id, int red, int green, int blue, int points) throws InvalidInputException {
+		
+		// Perform basic input validation to ensure the numeric values are valid.
+		
+		if (red > 255 || red < 0) {
+			throw new InvalidInputException("Red value not valid");
+		}
+		else if (green > 255 || green < 0) {
+			throw new InvalidInputException("Green value not valid");
+		}
+		else if (blue > 255 || blue < 0) {
+			throw new InvalidInputException("Blue value not valid");
+		}
+		else if (point > 1000 || point < 1) {
+			throw new InvalidInputException("Point value not valid");
+		}
+		
+		// Get the block list for the selected game.
+		
+		Game game = Block223Application.getCurrentGame();
+		if (game == null) {
+			throw new InvalidInputException("No game selected")
+		}
+		List<Block> blocks = game.getBlocks();
+		
+		// Find the desired block in the block list.
+		
+		Block foundBlock == null;
+		for (Block block : blocks) {
+			int blockID = block.getId();
+			if (blockID == id) {
+				foundBlock = block;
+				break;
+			}
+		}
+		
+		if (foundBlock == null) {
+			throw new InvalidInputException("Invalid block ID")
+		}
+		
+		// Update block data
+		
+		foundBlock.setRed(red);
+		foundBlock.setGreen(green);
+		foundBlock.setBlue(blue);
+		foundBlock.setPoints(points);
+		
 	}
 
 	public static void positionBlock(int id, int level, int gridHorizontalPosition, int gridVerticalPosition)
@@ -72,7 +136,7 @@ public class Block223Controller {
 
 	public static TOBlock getBlockOfCurrentDesignableGame(int id) throws InvalidInputException {
 	}
-
+	
 	public List<TOGridCell> getBlocksAtLevelOfCurrentDesignableGame(int level) throws InvalidInputException {
 	}
 
