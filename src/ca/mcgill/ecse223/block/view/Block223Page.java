5912496a6b48670550a4145cc80be9717007c002
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-02-24 21:40:43 -0500

    Update Block223Page.java

--- src/ca/mcgill/ecse223/block/view/Block223Page.java
+++ src/ca/mcgill/ecse223/block/view/Block223Page.java
@@ -4,4 +4,11 @@ public class Block223Page {
 
 /*Here are the java swing methods to be implemented after the meeting on Monday, 
 once we will have agreed on the uniformity of the UI*/
+  
+  ///Elements for add a block
+  /* - Add JButton, JSlider and ask the teacher about implementing the view that changes color as the colors get chosen to see what the block
+  looks like*/
+  
+ ///Elements for delete a block
+  /*Add JButton, JComboBox for the choice of ID*/
 }
