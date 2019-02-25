feef65446079e3176e5494c3506f88db0801195c
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-02-24 20:57:10 -0500

    Create Block223Page.java
    The Block223Page file for the UI of the project

---
+++ src/ca/mcgill/ecse223/block/view/Block223Page.java
@@ -0,0 +1,7 @@
+package ca.mcgill.ecse223.block.view;
+
+public class Block223Page {
+
+/*Here are the java swing methods to be implemented after the meeting on Monday, 
+once we will have agreed on the uniformity of the UI*/
+}
