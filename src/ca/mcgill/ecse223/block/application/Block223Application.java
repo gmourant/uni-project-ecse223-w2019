527ae7f05584e2c17070302fb325a480de07659f
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-02-25 21:28:18 -0500

    Update Block223Application.java

--- src/ca/mcgill/ecse223/block/application/Block223Application.java
+++ src/ca/mcgill/ecse223/block/application/Block223Application.java
@@ -1,27 +1,55 @@
 package ca.mcgill.ecse223.block.application;
 
-import ca.mcgill.ecse223.block.model.Block223;
+import ca.mcgill.ecse223.block.model.*;
+import ca.mcgill.ecse223.block.persistence.Block223Persistence;
 import ca.mcgill.ecse223.block.view.Block223Page;
 
 public class Block223Application {
-	
-private static Block223 block223;
-	
-	public static void main(String[] args) {
-		// start UI
+
+    public static Block223 block223;
+    public static UserRole currentUserRole;
+    public static Game currentGame;
+
+    public static void main(String[] args) {
+        // start UI
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
-                new Block223Page().setVisible(true); //This method will need to be changed to get the setVisible from the view java file
+                new Block223Page().setVisible(true);
             }
         });
-	}
+    }
 
-	public static Block223 getBlock223() {
-		if (block223 == null) {
-			// for now, we are just creating an empty block223app
-			block223 = new Block223();
-		}
- 		return block223;
-	}
+    public static Block223 resetBlock223() {
+        // Method that forces a load from the file
+		    // Return the root Block223 object
+        if (block223 == null) {
+            // load model
+           block223 = Block223Persistence.load();
+        }
+        return block223;
+    }
 
+    public static Block223 getBlock223() {
+        if (block223 == null) {
+            block223 = new Block223();
+        }
+        return block223;
+    }
+    
+    public static void setCurrentUserRole(UserRole aUserRole){
+        currentUserRole = aUserRole;
+    }
+    
+    public static UserRole getCurrentUserRole(){
+        return currentUserRole;
+    }
+    
+    public static void setCurrentGame(Game aGame){
+        currentGame = aGame;
+    }
+    
+    public static Game getCurrentGame(){
+        return currentGame;
+    }
 }
