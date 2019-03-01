d3bcce58d51e1c9faa7f38c4cbf551172fc53dae
Mat Bis <mathieu.bissonnette2@mail.mcgill.ca>
2019-02-28 21:41:41 -0500

    Added the PagePositionBlock.java
    It will implement the level editor (BlockAssignment management).

---
+++ src/ca/mcgill/ecse223/block/view/PagePositionBlock.java
@@ -0,0 +1,177 @@
+package ca.mcgill.ecse223.block.view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.Border;
+import javax.swing.border.MatteBorder;
+
+import test.test.EditorMode;
+import test.test.EditorPanel;
+import test.test.EditorPanel.CellPane;
+import test.test.EditorPanel.EditorGrid;
+
+public class PagePositionBlock extends ContentPage{
+	
+	enum EditorMode {
+    	ADD, CUT, PASTE, REMOVE
+    }
+	
+	public PagePositionBlock(Block223MainPage parent) {
+		super(parent);
+		initializeEditor();
+	    
+	    EditorMode editorMode = EditorMode.ADD;
+	}
+
+    public void initializeEditor() {
+                JFrame frame = new JFrame("Testing");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLayout(new BorderLayout());
+                frame.add(new EditorPanel());
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+    }
+    
+    public class EditorPanel extends JPanel {
+
+    	EditorPanel() {
+        	
+            setLayout(new BorderLayout());
+            add(new EditorGrid(), BorderLayout.CENTER);
+            
+            // Set up the tool radio buttons.
+            ButtonGroup toolbarGroup = new ButtonGroup();
+	            JRadioButton addButton = new JRadioButton("Add", true);
+	            addButton.setActionCommand("ADD");
+	            JRadioButton removeButton = new JRadioButton("Remove");
+	            removeButton.setActionCommand("REMOVE");
+	            JRadioButton moveButton = new JRadioButton("Move");
+	            moveButton.setActionCommand("MOVE");
+	            toolbarGroup.add(addButton);
+	            toolbarGroup.add(removeButton);
+	            toolbarGroup.add(moveButton);
+	        
+            // ComboBox block type selector.
+	            
+	        
+	        
+	        JComboBox blockList = new JComboBox();
+
+	        // Add elements to the toolbar.
+	        JPanel toolbar = new JPanel();
+	        toolbar.setLayout(new GridLayout(4,1));
+	        toolbar.add(blockList);
+	        toolbar.add(addButton);
+	        toolbar.add(removeButton);
+	        toolbar.add(moveButton);
+            add(toolbar, BorderLayout.EAST);
+        }
+  
+
+	    class EditorGrid extends JPanel {
+	
+	    	EditorGrid() {
+	        	
+	            setLayout(new GridBagLayout());
+	
+	            GridBagConstraints gbc = new GridBagConstraints();
+	            for (int row = 0; row < 8; row++) {
+	                for (int col = 0; col < 8; col++) {
+	                    gbc.gridx = col;
+	                    gbc.gridy = row;
+	
+	                    CellPane cellPane = new CellPane();
+	                    Border border = null;
+	                    if (row < 7) {
+	                        if (col < 7) {
+	                            border = new MatteBorder(1, 1, 0, 0, Color.GRAY);
+	                        } else {
+	                            border = new MatteBorder(1, 1, 0, 1, Color.GRAY);
+	                        }
+	                    } else {
+	                        if (col < 7) {
+	                            border = new MatteBorder(1, 1, 1, 0, Color.GRAY);
+	                        } else {
+	                            border = new MatteBorder(1, 1, 1, 1, Color.GRAY);
+	                        }
+	                    }
+	                    cellPane.setBorder(border);
+	                    add(cellPane, gbc);
+	                }
+	            }
+	        }
+	    }
+	
+	    class CellPane extends JPanel {
+	
+	        private Color defaultBackground;
+	        private boolean empty = true;
+	
+	        public CellPane() {
+	            addMouseListener(new MouseAdapter() {
+	                @Override
+					                public void mouseEntered(MouseEvent e) {
+					                    defaultBackground = getBackground();
+					                    if (empty)
+					                    	if (editorMode == EditorMode.PASTE) {
+					                    		setBackground(Color.GRAY);
+					                		} else {
+					                    		setBackground(Color.LIGHT_GRAY);
+					                		}
+					                }
+					
+					                @Override
+					                public void mouseExited(MouseEvent e) {
+					                	if (empty)
+					                		setBackground(defaultBackground);
+					                }
+					                
+					                @Override
+					                public void mousePressed(MouseEvent e) {
+					                    switch (editorMode) {
+					                    case ADD: {
+						                	setBackground(Color.RED);
+						                    empty = false;
+					                    }
+					                    }
+					                    repaint();
+					                }
+					            });
+					        }
+					
+					        @Override
+					        public Dimension getPreferredSize() {
+					            return new Dimension(50, 50);
+					        }
+					    }
+				    }
+				    
+				    class RadioListener implements ActionListener {
+				    	public void actionPerformed(ActionEvent e) {
+				    		switch (e.getActionCommand()) {
+				    			case "ADD": editorMode = EditorMode.ADD;
+				    			case "REMOVE": editorMode = EditorMode.REMOVE;
+				    			case "MOVE": editorMode = EditorMode.CUT;
+				    			default: break;
+				    		}
+					    	}
+}
