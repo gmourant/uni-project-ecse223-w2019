8b53b947ea5e962d8d0fa3fd28f55f17f4110397
Kelly Ma <kelly.ma@mail.mcgill.ca>
2019-03-04 12:34:18 -0500

    modified name generator

--- src/ca/mcgill/ecse223/block/view/RandomNameGenerator.java
+++ src/ca/mcgill/ecse223/block/view/RandomNameGenerator.java
@@ -2,6 +2,11 @@ package ca.mcgill.ecse223.block.view;
 
 import java.util.Random;
 
+/**
+ * A random name generator used to populate the
+ * name field with suggestions on PageAddGame.java
+ * @author Kelly Ma
+ */
 public class RandomNameGenerator {
 
    private String[] beginning = { "Kr", "Ca", "Ra", "Mrok", "Cru",
@@ -13,9 +18,9 @@ public class RandomNameGenerator {
          "marac", "zoir", "slamar", "salmar", "urak" };
    private String[] end = { "d", "ed", "ark", "arc", "es", "er", "der",
          "tron", "med", "ure", "zur", "cred", "mur" };
-   private String[] game = {" Destructor", " Terminator", " the Only", " Race",
+   private String[] game = {" Destructor", " Terminator", ", the Ultimate Test", " Race",
 		   " le Defi", ", an Experience", ", Meet Your Destiny", ", Play if you Dare",
-		   ", Test your Wits", ", Noobs Only"};
+		   ", Test your Wits", ", Noobs Only", ", Explore the Unknown"};
    private Random rand = new Random();
 
    public String generateName() {
