759f659cf25fffc8cd23c0829dda3f4a08db6c0c
SofiaDieguez <sofia.dieguez@mail.mcgill.ca>
2019-03-04 13:14:50 -0500

    add import for JList

--- src/ca/mcgill/ecse223/block/view/PageLogin.java
+++ src/ca/mcgill/ecse223/block/view/PageLogin.java
@@ -16,6 +16,7 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
