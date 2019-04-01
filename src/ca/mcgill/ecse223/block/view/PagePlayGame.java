ad6afa9e86d2e250a72796778a7bd9984d97eb03
Kelly Ma <kelly.ma@mail.mcgill.ca>
2019-04-01 11:16:39 -0400

    started

---
+++ src/ca/mcgill/ecse223/block/view/PagePlayGame.java
@@ -0,0 +1,25 @@
+package ca.mcgill.ecse223.block.view;
+
+import java.awt.GridLayout;
+import ca.mcgill.ecse223.block.application.Block223Application;
+import ca.mcgill.ecse223.block.controller.Block223Controller;
+import ca.mcgill.ecse223.block.controller.InvalidInputException;
+
+public class PagePlayGame extends ContentPage implements Block223PlayModeInterface {
+
+	public PagePlayGame(Block223MainPage parent) {
+		// Inherit features from ContentPage
+		super(parent);
+		setLayout(new GridLayout(10,1));
+	    add(createHeader(Block223Application.getCurrentPlayableGame().getGame().getName())); 
+	}
+	
+	public String takeInputs() {
+		
+	}
+	
+	public void refresh() {
+		
+	}
+	
+}
